2d0d955a82621a53717eae7d2b2f8c055298f8f4
Artificer-Cat <chemestru@gmail.com>
2026-03-28 23:57:05 +0300

    add docker-compose file, add dockerfiles to build test-apps, add properties files for kafka cluster, add sturtup script

---
+++ cmd/producer/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	kafka "github.com/confluentinc/confluent-kafka-go/v2/kafka"
+)
+
+func main() {
+
+	time.Sleep(5 * time.Second)
+
+
+	bootstrapServers := "kafka-server-alpha:9092"
+	topic := "test"
+	totalMsgcnt := 3
+
+	p, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": bootstrapServers})
+
+	if err != nil {
+		fmt.Printf("Failed to create producer: %s\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Created Producer %v\n", p)
+
+	go func() {
+		for e := range p.Events() {
+			switch ev := e.(type) {
+			case *kafka.Message:
+				m := ev
+				if m.TopicPartition.Error != nil {
+					fmt.Printf("Delivery failed: %v\n", m.TopicPartition.Error)
+				} else {
+					fmt.Printf("Delivered message to topic %s [%d] at offset %v\n",
+						*m.TopicPartition.Topic, m.TopicPartition.Partition, m.TopicPartition.Offset)
+				}
+			case kafka.Error:
+				fmt.Printf("Error: %v\n", ev)
+			default:
+				fmt.Printf("Ignored event: %s\n", ev)
+			}
+		}
+	}()
+
+	msgcnt := 0
+	for msgcnt < totalMsgcnt {
+		value := fmt.Sprintf("Producer example, message #%d", msgcnt)
+
+		err = p.Produce(&kafka.Message{
+			TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+			Value:          []byte(value),
+			Headers:        []kafka.Header{{Key: "myTestHeader", Value: []byte("header values are binary")}},
+		}, nil)
+
+		if err != nil {
+			if err.(kafka.Error).Code() == kafka.ErrQueueFull {
+				time.Sleep(time.Second)
+				continue
+			}
+			fmt.Printf("Failed to produce message: %v\n", err)
+		}
+		msgcnt++
+	}
+
+	for p.Flush(10000) > 0 {
+		fmt.Print("Still waiting to flush outstanding messages\n")
+	}
+	p.Close()
+}
